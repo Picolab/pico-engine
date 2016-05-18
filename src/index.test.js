b7797a68979f49f745a4e303ab66048a0ae40c83
Matthew Wright <matthew@smallhelm.com>
2016-05-18 15:57:26 -0600

    fleshing out index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,27 +1,48 @@
+var _ = require('lodash');
+var λ = require('contra');
 var test = require('tape');
-var memdown = require('memdown');
-var PicoEngine = require('./');
+var mkTestPicoEngine = require('./mkTestPicoEngine');
 
-test('something', function(t){
-  var pe = PicoEngine({db: {db: memdown}});
+test('PicoEngine - the basics', function(t){
+  var pe = mkTestPicoEngine();
 
-  pe.db.toObj(function(err, db_data){
+  λ.series([
+    λ.curry(pe.db.toObj),
+
+    λ.curry(pe.db.newPico, {}),
+    λ.curry(pe.db.newChannel, {pico_id: 'id0', name: 'one', type: 't'}),
+    λ.curry(pe.db.addRuleset, {pico_id: 'id0', rid: 'rid1x0'}),
+    λ.curry(pe.db.addRuleset, {pico_id: 'id0', rid: 'rid2x0'}),
+
+    λ.curry(pe.queryFn, 'id1', 'rid1x0', 'hello', {obj: 'Bob'}),
+
+    λ.curry(pe.db.toObj)
+  ], function(err, results){
     if(err) return t.end(err);
-    t.deepEquals(db_data, {});
 
-    pe.db.newPico({}, function(err, new_pico){
-      if(err) return t.end(err);
+    t.deepEquals(_.head(results), {});
 
-      pe.db.toObj(function(err, db_data){
-        if(err) return t.end(err);
-        var exp = {};
-        exp.pico = {};
-        exp.pico[new_pico.id] = new_pico;
+    t.deepEquals(results[5], 'Hello Bob');
 
-        t.deepEquals(db_data, exp);
-        t.end();
-      });
+    t.deepEquals(_.last(results), {
+      pico: {
+        'id0': {
+          id: 'id0',
+          channel: {
+            'id1': {
+              id: 'id1',
+              name: 'one',
+              type: 't'
+            }
+          },
+          ruleset: {
+            'rid1x0': {on: true},
+            'rid2x0': {on: true}
+          }
+        }
+      }
     });
-  });
 
+    t.end();
+  });
 });
