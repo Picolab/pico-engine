4d9d0624360879065e418a710fb9f707934c77b1
Matthew Wright <matthew@smallhelm.com>
2016-05-25 17:30:16 -0600

    fleshing out how the state machine is evaluated

--- src/selectRulesToEval.js
+++ src/selectRulesToEval.js
@@ -1,6 +1,22 @@
 var _ = require('lodash');
 var λ = require('contra');
 
+var evalExpr = function(fns, ctx, exp){
+  if(_.isArray(exp)){
+    if(exp[0] === 'not'){
+      return !evalExpr(fns, ctx, exp[1]);
+    }else if(exp[0] === 'and'){
+      return evalExpr(fns, ctx, exp[1]) && evalExpr(fns, ctx, exp[2]);
+    }else if(exp[0] === 'or'){
+      return evalExpr(fns, ctx, exp[1]) || evalExpr(fns, ctx, exp[2]);
+    }
+  }
+  return fns[exp](ctx);
+};
+
+//TODO should this be persisted in db so it's not tied to this process?
+var last_state_machine_state = {};
+
 module.exports = function(ctx, rulesets, callback){
   //TODO optimize by using the salience graph
 
@@ -13,25 +29,22 @@ module.exports = function(ctx, rulesets, callback){
       next(undefined, false);
       return;
     }
-    var curr_state = 'TODO';//TODO get this from memory
+    var key = [ctx.pico.id, rule.rid, rule.rule_name].join('-');
+    var curr_state = last_state_machine_state[key];
     if(!_.has(rule.select.state_machine, curr_state)){
       curr_state = 'start';
     }
     var stm = rule.select.state_machine[curr_state];
 
     var matching_pair = _.find(stm, function(s){
-      var exp = s[0];
-
-      if(_.isArray(exp)){
-        return false;//TODO
-      }
-      return rule.select.eventexprs[exp](ctx);
+      return evalExpr(rule.select.eventexprs, ctx, s[0]);
     });
     var next_state = matching_pair ? matching_pair[1] : undefined;
     if(next_state === 'end'){
+      last_state_machine_state[key] = 'start';//start from the begining next time
       next(undefined, true);
     }else{
-      //TODO store the state
+      last_state_machine_state[key] = next_state;
       next(undefined, false);
     }
   }, callback);
