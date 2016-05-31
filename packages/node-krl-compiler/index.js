4a8ae6153dc7fa5dd232059affb76b5fcd33545e
Matthew Wright <matthew@smallhelm.com>
2016-05-31 13:20:51 -0600

    starting to use escodegen

--- packages/node-krl-compiler/index.js
+++ packages/node-krl-compiler/index.js
@@ -1,7 +1,43 @@
+var _ = require('lodash');
 var parser = require('krl-parser');
+var escodegen = require('escodegen');
 
 module.exports = function(src){
   var ast = parser(src);
 
-  return 'console.log("TODO");';
+  if(!_.isArray(ast) || ast.length !== 1 || ast[0].type !== 'ruleset'){
+    throw new Error('one ruleset per file');
+  }
+  var ruleset = ast[0];
+
+  var estree = {
+    "type": "Program",
+    "body": [
+      {
+        "type": "ExpressionStatement",
+        "expression": {
+          "type": "AssignmentExpression",
+          "operator": "=",
+          "left": {
+            "type": "MemberExpression",
+            "computed": false,
+            "object": {
+              "type": "Identifier",
+              "name": "module"
+            },
+            "property": {
+              "type": "Identifier",
+              "name": "exports"
+            }
+          },
+          "right": {
+            "type": "ObjectExpression",
+            "properties": [
+            ]
+          }
+        }
+      }
+    ]
+  };
+  return escodegen.generate(estree);
 };
