c337b615c8cf031984883eb71e83402c0beda5e8
Matthew Wright <matthew@smallhelm.com>
2016-06-23 10:29:02 -0600

    cleanup

--- packages/node-krl-compiler/src/compile.js
+++ packages/node-krl-compiler/src/compile.js
@@ -92,18 +92,16 @@ module.exports = function(ast, options){
   options = options || {};
 
   var compile = function compile(ast){
-    if(!ast){
-      throw new Error('Invalid ast node: ' + JSON.stringify(ast));
-    }
     if(_.isArray(ast)){
       return _.map(ast, function(a){
         return compile(a);
       });
+    }else if(!_.has(ast, 'type')){
+      throw new Error('Invalid ast node: ' + JSON.stringify(ast));
+    }else if(!_.has(comp_by_type, ast.type)){
+      throw new Error('Unsupported ast node type: ' + ast.type);
     }
-    if(_.has(comp_by_type, ast.type)){
-      return comp_by_type[ast.type](ast, compile);
-    }
-    throw new Error('Unsupported ast node type: ' + ast.type);
+    return comp_by_type[ast.type](ast, compile);
   };
 
   return compile(ast, 0);
