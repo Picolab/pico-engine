01f3363628f8ff0dc43c29902518e7a90887224a
Matthew Wright <matthew@smallhelm.com>
2016-06-27 14:06:32 -0600

    dont use shorthand for e

--- packages/node-krl-compiler/src/compile.js
+++ packages/node-krl-compiler/src/compile.js
@@ -4,10 +4,10 @@ var mkTree = require('estree-builder');
 
 var comp_by_type = {
   'String': function(ast, comp, e){
-    return e.str(ast.value);
+    return e('string', ast.value);
   },
   'Identifier': function(ast, comp, e){
-    return e.id(toId(ast.value));
+    return e('id', toId(ast.value));
   },
   'Application': function(ast, comp, e){
     return e('call',
@@ -24,14 +24,14 @@ var comp_by_type = {
       rules_obj[rule.name.value] = comp(rule);
     });
     return [
-      e(';', e('=', e('.', e.id('module'), e.id('exports')), e.obj({
-        name: e.str(ast.name.value),
-        rules: e.obj(rules_obj)
+      e(';', e('=', e('.', e('id', 'module'), e('id', 'exports')), e('obj', {
+        name: e('str', ast.name.value),
+        rules: e('obj', rules_obj)
       })))
     ];
   },
   'Rule': function(ast, comp, e){
-    return e.obj({
+    return e('obj', {
       select: comp(ast.select),
       action: comp(ast.action_block)
     });
@@ -40,19 +40,19 @@ var comp_by_type = {
     ast = ast.event;//TODO remove this Hack
 
     var estCTXEventProp = function(prop){
-      return e('.', e('.', e.id('ctx'), e.id('event')), e.id(prop));
+      return e('.', e('.', e('id', 'ctx'), e('id', 'event')), e('id', prop));
     };
 
     var eventExprToEstree = function(expr){
       var fn_body = [];
-      fn_body.push(e(';', e('call', e.id('callback'), [
-        e.nil(),
+      fn_body.push(e(';', e('call', e('id', 'callback'), [
+        e('nil'),
         e('&&',
-          e('===', estCTXEventProp('domain'), e.str('echo')),
-          e('===', estCTXEventProp('type'), e.str('hello'))
+          e('===', estCTXEventProp('domain'), e('str', 'echo')),
+          e('===', estCTXEventProp('type'), e('str', 'hello'))
         )
       ])));
-      return e.fn(['ctx', 'callback'], fn_body);
+      return e('fn', ['ctx', 'callback'], fn_body);
     };
 
 
@@ -74,24 +74,24 @@ var comp_by_type = {
       state_machine.start.push([['not', id], 'start']);
     });
 
-    return e.obj({
-      graph: e.json(graph),
-      eventexprs: e.obj(eventexprs),
-      state_machine: e.json(state_machine)
+    return e('obj', {
+      graph: e('json', graph),
+      eventexprs: e('obj', eventexprs),
+      state_machine: e('json', state_machine)
     });
   },
   'RuleActionBlock': function(ast, comp, e){
-    return e.fn(['ctx', 'callback'], _.flattenDeep(_.map(ast.actions, function(action){
+    return e('fn', ['ctx', 'callback'], _.flattenDeep(_.map(ast.actions, function(action){
       return comp(action);
     })));
   },
   'RuleAction': function(ast, comp, e){
-    return e(';', e('call', e.id('callback'), [
-      e.nil(),
-      e.obj({
-        type: e.str('directive'),
-        name: e.str(ast.args[0].value),
-        options: e.obj(_.fromPairs(_.map(ast['with'], function(dec){
+    return e(';', e('call', e('id', 'callback'), [
+      e('nil'),
+      e('obj', {
+        type: e('str', 'directive'),
+        name: e('str', ast.args[0].value),
+        options: e('obj', _.fromPairs(_.map(ast['with'], function(dec){
           return [dec.left.value, comp(dec.right)];
         })))
       })
@@ -105,16 +105,10 @@ module.exports = function(ast, options){
   var toLoc = options.toLoc || _.noop;
 
   var e = function(){
-    var args = Array.prototype.slice.call(arguments, 0, arguments.length);
+    var args = Array.prototype.slice.call(arguments);
     toLoc(0, 1);
     return mkTree.apply(null, args);
   };
-  _.each(mkTree, function(fn, key){
-    e[key] = function(){
-      var args = Array.prototype.slice.call(arguments, 0, arguments.length);
-      return mkTree.apply(null, [key].concat(args));
-    };
-  });
 
   var compile = function compile(ast){
     if(_.isArray(ast)){
