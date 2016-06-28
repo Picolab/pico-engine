672c41401addb10f5ee93628bf9303e53bbf6df2
Matthew Wright <matthew@smallhelm.com>
2016-06-28 11:03:27 -0600

    test for ((a and b) or c)

--- packages/node-krl-compiler/test-files/events.js
+++ packages/node-krl-compiler/test-files/events.js
@@ -105,6 +105,85 @@ module.exports = {
           ]
         }
       }
+    },
+    'and_or': {
+      'select': {
+        'graph': {
+          'echo': {
+            'a': { 'expr_0': true },
+            'b': { 'expr_1': true },
+            'c': { 'expr_2': true }
+          }
+        },
+        'eventexprs': {
+          'expr_0': function (ctx, callback) {
+            callback(undefined, ctx.event.domain === 'echo' && ctx.event.type === 'a');
+          },
+          'expr_1': function (ctx, callback) {
+            callback(undefined, ctx.event.domain === 'echo' && ctx.event.type === 'b');
+          },
+          'expr_2': function (ctx, callback) {
+            callback(undefined, ctx.event.domain === 'echo' && ctx.event.type === 'c');
+          }
+        },
+        'state_machine': {
+          'start': [
+            [
+              'expr_0',
+              'state_0'
+            ],
+            [
+              'expr_1',
+              'state_1'
+            ],
+            [
+              'expr_2',
+              'end'
+            ],
+            [
+              [
+                'not',
+                [
+                  'or',
+                  'expr_0',
+                  [
+                    'or',
+                    'expr_1',
+                    'expr_2'
+                  ]
+                ]
+              ],
+              'start'
+            ]
+          ],
+          'state_0': [
+            [
+              'expr_1',
+              'end'
+            ],
+            [
+              [
+                'not',
+                'expr_1'
+              ],
+              'state_0'
+            ]
+          ],
+          'state_1': [
+            [
+              'expr_0',
+              'end'
+            ],
+            [
+              [
+                'not',
+                'expr_0'
+              ],
+              'state_1'
+            ]
+          ]
+        }
+      }
     }
   }
 };
--- packages/node-krl-compiler/test-files/events.krl
+++ packages/node-krl-compiler/test-files/events.krl
@@ -5,4 +5,7 @@ ruleset io.picolabs.events {
   rule and_op {
     select when echo hello and say hello
   }
+  rule and_or {
+    select when (echo a and echo b) or echo c
+  }
 }
