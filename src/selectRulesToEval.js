31231162634f910e125fdfea7dce2a5ed8d66cd4
Matthew Wright <matthew@smallhelm.com>
2016-07-01 15:58:34 -0600

    cleaning up selectRulesToEval

--- src/selectRulesToEval.js
+++ src/selectRulesToEval.js
@@ -1,61 +1,34 @@
 var _ = require('lodash');
 var λ = require('contra');
-var contraFind = require('contra-find');
 var applyInFiber = require('./applyInFiber');
 
 var evalExpr = function(ctx, exp, callback){
   if(_.isArray(exp)){
     if(exp[0] === 'not'){
-      evalExpr(ctx, exp[1], function(err, is_match){
-        if(err) return callback(err);
-        callback(undefined, !is_match);
-      });
-      return;
+      return !evalExpr(ctx, exp[1]);
     }else if(exp[0] === 'and'){
-      evalExpr(ctx, exp[1], function(err, is_a){
-        if(err) return callback(err);
-        if(!is_a){
-          callback(undefined, false);
-          return;
-        }
-        evalExpr(ctx, exp[2], callback);
-      });
-      return;
+      return evalExpr(ctx, exp[1]) && evalExpr(ctx, exp[2]);
     }else if(exp[0] === 'or'){
-      evalExpr(ctx, exp[1], function(err, is_a){
-        if(err) return callback(err);
-        if(is_a){
-          callback(undefined, true);
-          return;
-        }
-        evalExpr(ctx, exp[2], callback);
-      });
-      return;
+      return evalExpr(ctx, exp[1]) || evalExpr(ctx, exp[2]);
     }
   }
   //only run the function if the domain and type match
   var domain = ctx.event.domain;
   var type = ctx.event.type;
   if(_.get(ctx, ['rule', 'select', 'graph', domain, type, exp]) !== true){
-    return callback(undefined, false);
+    return false;
   }
-  applyInFiber(ctx.rule.select.eventexprs[exp], null, [ctx], callback);
+  return ctx.rule.select.eventexprs[exp](ctx);
 };
 
-var getNextState = function(ctx, curr_state, callback){
-  //TODO if we are going to stick with fibers
-  //TODO then remove contraFind and the async evalExpr
+var getNextState = function(ctx, curr_state){
   var stm = ctx.rule.select.state_machine[curr_state];
 
-  contraFind(λ, stm, function(s, next){
-    evalExpr(ctx, s[0], next);
-  }, function(err, matching_pair){
-    if(err) return callback(err);
-
-    var next_state = matching_pair ? matching_pair[1] : undefined;
-
-    callback(undefined, next_state);
+  var matching_pair = _.find(stm, function(s){
+    return evalExpr(ctx, s[0]);
   });
+
+  return matching_pair ? matching_pair[1] : undefined;
 };
 
 module.exports = function(ctx, salience_graph, rulesets, callback){
@@ -84,10 +57,10 @@ module.exports = function(ctx, salience_graph, rulesets, callback){
     ctx.db.getStateMachineState(ctx.pico.id, rule, function(err, curr_state){
       if(err) return next(err);
 
-      getNextState(_.assign({}, ctx, {
+      applyInFiber(getNextState, null, [_.assign({}, ctx, {
         rule: rule,
         scope: rule.scope
-      }), curr_state, function(err, next_state){
+      }), curr_state], function(err, next_state){
         if(err) return next(err);
 
         ctx.db.putStateMachineState(ctx.pico.id, rule, next_state, function(err){
