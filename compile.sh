a7ad33ddd00fc75cd84a1f218c3fadebbf7fbf7d
Matthew Wright <matthew@smallhelm.com>
2016-07-06 14:25:17 -0600

    fixing compile.sh

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@
 #exit ASAP if anything fails
 set -e
 
-node_modules/.bin/krl-compiler < test-rulesets/events.krl > src/rulesets/events.js 
-node_modules/.bin/krl-compiler < test-rulesets/hello-world.krl > src/rulesets/hello-world.js 
-node_modules/.bin/krl-compiler < test-rulesets/persistent.krl > src/rulesets/persistent.js 
-node_modules/.bin/krl-compiler < test-rulesets/scope.krl > src/rulesets/scope.js
-node_modules/.bin/krl-compiler < test-rulesets/methods.krl > src/rulesets/methods.js
+node_modules/.bin/krl-compiler < test-rulesets/events.krl > test-rulesets/events.js
+node_modules/.bin/krl-compiler < test-rulesets/hello-world.krl > test-rulesets/hello-world.js
+node_modules/.bin/krl-compiler < test-rulesets/persistent.krl > test-rulesets/persistent.js
+node_modules/.bin/krl-compiler < test-rulesets/scope.krl > test-rulesets/scope.js
+node_modules/.bin/krl-compiler < test-rulesets/methods.krl > test-rulesets/methods.js
