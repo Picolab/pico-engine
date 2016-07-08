b8c3266b388866dc11a2949234ba40b98ef86b36
Matthew Wright <matthew@smallhelm.com>
2016-07-08 11:28:24 -0600

    fleshing out fromJS

--- src/krl/fromJS.js
+++ src/krl/fromJS.js
@@ -1,6 +1,9 @@
 var _ = require('lodash');
+var KRLNull = require('./types/Null');
 var KRLType = require('./types/KRLType');
 var KRLString = require('./types/String');
+var KRLNumber = require('./types/Number');
+var KRLRegExp = require('./types/RegExp');
 
 var fromJS = function fromJS(val){
   if(val instanceof KRLType){
@@ -12,8 +15,17 @@ var fromJS = function fromJS(val){
   if(_.isPlainObject(val)){
     return _.mapValues(val, fromJS);
   }
+  if(_.isRegExp(val)){
+    return new KRLRegExp(val);
+  }
   if(_.isString(val)){
     return new KRLString(val);
+  }
+  if(_.isNaN(val)){
+    return new KRLNull(val);
+  }
+  if(_.isNumber(val)){
+    return new KRLNumber(val);
   }
   return val;
 };
--- src/krl/types/RegExp.js
+++ src/krl/types/RegExp.js
@@ -1,6 +1,11 @@
+var _ = require('lodash');
 var makeType = require('./makeType');
 
 module.exports = makeType(function(pattern, flags){
+  if(_.isRegExp(pattern)){
+    this.value = pattern;
+    return;
+  }
   this.value = new RegExp(pattern, flags);
 }, {
 });
