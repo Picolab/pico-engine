07058cea138649cccae5aec2eedd3bef2637cefa
Matthew Wright <matthew@smallhelm.com>
2016-07-08 13:50:17 -0600

    syncing test files with pico-engine

--- packages/node-krl-compiler/test-files/operators.js
+++ packages/node-krl-compiler/test-files/operators.js
@@ -1,8 +1,16 @@
 module.exports = {
   'name': 'io.picolabs.operators',
-  'meta': { 'shares': ['cap_hello'] },
+  'meta': { 'shares': ['results'] },
   'global': function (ctx) {
-    ctx.scope.set('cap_hello', ctx.krl.stdlib['capitalize']('Hello World'));
+    ctx.scope.set('nothing', ctx.scope.get('null'));
+    ctx.scope.set('results', {
+      'str_as_num': ctx.krl.stdlib['as']('100.25', 'Number'),
+      'num_as_str': ctx.krl.stdlib['as'](1.05, 'String'),
+      'regex_as_str': ctx.krl.stdlib['as'](new RegExp('blah', 'i'), 'String'),
+      'isnull': ctx.krl.stdlib['isnull'](ctx.scope.get('nothing')),
+      'hello_cap': ctx.krl.stdlib['capitalize']('Hello World'),
+      'hello_low': ctx.krl.stdlib['lc']('Hello World')
+    });
   },
   'rules': {}
 };
--- packages/node-krl-compiler/test-files/operators.krl
+++ packages/node-krl-compiler/test-files/operators.krl
@@ -1,8 +1,16 @@
 ruleset io.picolabs.operators {
   meta {
-    shares cap_hello
+    shares results
   }
   global {
-    cap_hello = "Hello World".capitalize()
+    nothing = null
+    results = {
+      "str_as_num": "100.25".as("Number"),
+      "num_as_str": 1.05.as("String"),
+      "regex_as_str": re#blah#i.as("String"),
+      "isnull": nothing.isnull(),
+      "hello_cap": "Hello World".capitalize(),
+      "hello_low": "Hello World".lc()
+    }
   }
 }
