d99a2af3df788c77cb57d4907f19af7e4b6c8304
Matthew Wright <matthew@smallhelm.com>
2016-08-29 10:06:33 -0500

    compile.sh loops through all krl files instead of hard-coding

--- packages/node-pico-engine-core/compile.sh
+++ packages/node-pico-engine-core/compile.sh
@@ -3,10 +3,8 @@
 #exit ASAP if anything fails
 set -e
 
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/events.krl > test-rulesets/events.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/hello-world.krl > test-rulesets/hello-world.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/persistent.krl > test-rulesets/persistent.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/scope.krl > test-rulesets/scope.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/operators.krl > test-rulesets/operators.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/chevron.krl > test-rulesets/chevron.js
-node_modules/.bin/krl-compiler --no-source-map < test-rulesets/section.krl > test-rulesets/section.js
+for f in test-rulesets/*.krl;
+do
+  out=$(echo $f | sed "s/\.krl$/\.js/")
+  node_modules/.bin/krl-compiler --no-source-map < $f > $out
+done
