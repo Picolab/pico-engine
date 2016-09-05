bde4307d226e74eaef93f07bf15d2f221de95d27
Matthew Wright <matthew@smallhelm.com>
2016-09-05 17:05:03 -0500

    adding test execution-order.krl

---
+++ packages/node-krl-compiler/test-rulesets/execution-order.js
@@ -0,0 +1,103 @@
+module.exports = {
+  "rid": "io.picolabs.execution-order",
+  "meta": { "shares": ["getOrder"] },
+  "global": function (ctx) {
+    ctx.scope.set("getOrder", ctx.krl.Closure(ctx, function (ctx) {
+      return ctx.modules.get("ent", "order");
+    }));
+  },
+  "rules": {
+    "first": {
+      "name": "first",
+      "select": {
+        "graph": { "execution_order": { "all": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [
+            [
+              "expr_0",
+              "end"
+            ],
+            [
+              [
+                "not",
+                "expr_0"
+              ],
+              "start"
+            ]
+          ]
+        }
+      },
+      "action_block": {
+        "actions": [{
+            "action": function (ctx) {
+              return {
+                "type": "directive",
+                "name": "first",
+                "options": {}
+              };
+            }
+          }]
+      },
+      "postlude": {
+        "fired": function (ctx) {
+          ctx.modules.set("ent", "order", ctx.krl.stdlib["append"](ctx.modules.get("ent", "order"), "first-fired"));
+        },
+        "notfired": undefined,
+        "always": function (ctx) {
+          ctx.modules.set("ent", "order", ctx.krl.stdlib["append"](ctx.modules.get("ent", "order"), "first-finally"));
+        }
+      }
+    },
+    "second": {
+      "name": "second",
+      "select": {
+        "graph": { "execution_order": { "all": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [
+            [
+              "expr_0",
+              "end"
+            ],
+            [
+              [
+                "not",
+                "expr_0"
+              ],
+              "start"
+            ]
+          ]
+        }
+      },
+      "action_block": {
+        "actions": [{
+            "action": function (ctx) {
+              return {
+                "type": "directive",
+                "name": "second",
+                "options": {}
+              };
+            }
+          }]
+      },
+      "postlude": {
+        "fired": function (ctx) {
+          ctx.modules.set("ent", "order", ctx.krl.stdlib["append"](ctx.modules.get("ent", "order"), "second-fired"));
+        },
+        "notfired": undefined,
+        "always": function (ctx) {
+          ctx.modules.set("ent", "order", ctx.krl.stdlib["append"](ctx.modules.get("ent", "order"), "second-finally"));
+        }
+      }
+    }
+  }
+};
