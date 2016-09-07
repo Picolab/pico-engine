95a16ce9d95f8cabcac44e2ac94c4bc9b185b52c
Matthew Wright <matthew@smallhelm.com>
2016-09-07 10:21:12 -0500

    updating test-rulesets/

--- packages/node-krl-compiler/test-rulesets/module-used.js
+++ packages/node-krl-compiler/test-rulesets/module-used.js
@@ -24,8 +24,13 @@ module.exports = {
         "graph": { "module_used": { "say_hello": { "expr_0": true } } },
         "eventexprs": {
           "expr_0": function (ctx) {
-            var matches = [];
-            ctx.scope.set("my_name", matches[0]);
+            var matches = ctx.event.getAttrMatches([[
+                "name",
+                new RegExp("(.*)", "")
+              ]]);
+            if (!matches)
+              return false;
+            ctx.scope.set("name", matches[0]);
             return true;
           }
         },
@@ -55,6 +60,80 @@ module.exports = {
                   "something": ctx.modules.get(ctx, "my_module", "hello")(ctx, [ctx.scope.get("name")]),
                   "configured": ctx.modules.get(ctx, "my_module_conf", "hello")(ctx, [ctx.scope.get("name")])
                 }
+              };
+            }
+          }]
+      }
+    },
+    "privateFn": {
+      "name": "privateFn",
+      "select": {
+        "graph": { "module_used": { "privateFn": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [
+            [
+              "expr_0",
+              "end"
+            ],
+            [
+              [
+                "not",
+                "expr_0"
+              ],
+              "start"
+            ]
+          ]
+        }
+      },
+      "action_block": {
+        "actions": [{
+            "action": function (ctx) {
+              return {
+                "type": "directive",
+                "name": "privateFn",
+                "options": { "something": ctx.modules.get(ctx, "my_module", "privateFn")(ctx, ["{{name}}"]) }
+              };
+            }
+          }]
+      }
+    },
+    "queryFn": {
+      "name": "queryFn",
+      "select": {
+        "graph": { "module_used": { "queryFn": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [
+            [
+              "expr_0",
+              "end"
+            ],
+            [
+              [
+                "not",
+                "expr_0"
+              ],
+              "start"
+            ]
+          ]
+        }
+      },
+      "action_block": {
+        "actions": [{
+            "action": function (ctx) {
+              return {
+                "type": "directive",
+                "name": "queryFn",
+                "options": { "something": ctx.modules.get(ctx, "my_module", "queryFn")(ctx, ["{{name}}"]) }
               };
             }
           }]
--- packages/node-krl-compiler/test-rulesets/module-used.krl
+++ packages/node-krl-compiler/test-rulesets/module-used.krl
@@ -7,9 +7,19 @@ ruleset io.picolabs.module-used {
       with greeting = "Greetings "
   }
   rule say_hello {
-    select when module_used say_hello setting(my_name);
+    select when module_used say_hello name re#(.*)# setting(name);
     send_directive("say_hello") with
       something = my_module:hello(name)
       configured = my_module_conf:hello(name)
+  }
+  rule privateFn {
+    select when module_used privateFn;
+    send_directive("privateFn") with
+      something = my_module:privateFn("{{name}}")
+  }
+  rule queryFn {
+    select when module_used queryFn;
+    send_directive("queryFn") with
+      something = my_module:queryFn("{{name}}")
   }
 }
