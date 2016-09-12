d319e5ace2cce220160ecc4a7cd3e74c9624889b
Bruce Conrad <bruce_conrad@byu.edu>
2016-09-12 09:12:38 -0600

    fully automated bootstrap

--- README.md
+++ README.md
@@ -19,3 +19,53 @@ The server is configured via some environment variables.
 
 ## License
 MIT
+
+## Starting the pico-engine
+
+Alternate technique for installaing the pico-engine,
+appropriate for those who wish to contribute
+improvements:
+
+Install by:
+ * `git clone https://github.com/Picolab/node-pico-engine`
+ * `npm install`
+
+Start with:
+ * `npm start`
+
+## UI
+Once you have started the pico-engine, 
+there is a web server running on your local machine.
+
+## Bootstrap
+Visit the Pico Bootstrap page at `localhost:8080`
+(assuming you use the default PORT).
+As it loads, the page will automatically perform
+all the operations necessary to
+create a root Pico and install two rulesets.
+
+There are two rulesets used by all Picos:
+ * `io.picolabs.pico` is used by each Pico to keep track of itself and its children
+ * `io.picolabs.visual_params` is used by each Pico to keep track of it in the My Picos page
+ 
+## Using the My Picos page
+
+With the rulesets installed, you can drag the rounded rectangle of your Pico and drop it
+wherever you want it. In its "About" tab (click on it to reveal the tabs) you can change its
+display name and color.
+
+Also in the "About" tab, you can add and delete child Picos.
+
+In the "Rulesets" tab you can see the information held for your Pico by each of its rulesets.
+By clicking on a ruleset id,
+you will be taken to the Engine Rulesets page
+where you can see its source code.
+
+To make your own ruleset, write your code in the box in the
+Engine Rulesets page.
+Use the "validate" button until the code compiles.
+Then use the "register" button to register this version
+of your code with the engine.
+Use the "enable" button to enable this version,
+and the "install" button to have the engine save away
+the compiled code for use.
--- public/js/index.js
+++ public/js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
             function(c){
               if (c && c.id) {
                 log("Owner Pico ECI: "+c.id);
-                callback(c.id);
+                callback(d.id,c.id);
               } else {
                 log("*Problem creating owner Pico channel");
               }
@@ -30,16 +30,60 @@ $(document).ready(function() {
       }
     });
   };
-  var installRuleset = function(rid,callback){
-    log("Getting "+rid);
+  var hashForRid = function(rid,callback){
+    log("Getting hash for "+rid);
+    $.getJSON("/api/db-dump", function(db_dump){
+      var hashobj;
+      for (var vds in db_dump.rulesets.versions[rid]) {
+        hashobj = db_dump.rulesets.versions[rid][vds];
+      }
+      if (hashobj) {
+        for(var hash in hashobj)
+        {
+          log(rid+" hash is "+hash);
+          callback(hash);
+          return;
+        }
+      }
+      log("*Problem getting hash for "+rid);
+    });
+  };
+  var installAndAddRuleset = function(rid,id,callback){
+    log("Getting "+rid+".krl");
     $.get("https://raw.githubusercontent.com/Picolab/node-pico-engine/master/krl/"+rid+".krl",function(k){
       if (k && k.length > 0) {
         log(rid+".krl length: "+k.length);
         log("Registering "+rid);
-        $.getJSON("/api/ruleset/register",{"src":k},function(r){
-          if (r && r.ok) {
+        $.getJSON("/api/ruleset/register",{"src":k},function(rr){
+          if (rr && rr.ok) {
             log(rid+" registered");
-            callback();
+            hashForRid(rid,function(hash){
+              log("Enabling "+rid);
+              $.getJSON("/api/ruleset/enable/"+hash,function(re){
+                if (re && re.ok) {
+                  log(rid+" enabled");
+                  log("Installing "+rid);
+                  $.getJSON("/api/ruleset/install/"+rid,function(ri){
+                    if (ri && ri.ok) {
+                      log(rid+" installed");
+                      log("Adding "+rid+" to pico "+id);
+                      $.getJSON("/api/pico/"+id+"/add-ruleset?rid="+rid,function(ra){
+                        if (ra && ra.ok) {
+                          log(rid+" added to pico "+id);
+                          callback();
+                        } else {
+                          log("*Problem adding "+rid);
+                        }
+                      });
+                    } else {
+                      log("*Problem installing "+rid);
+                    }
+                  });
+                } else {
+                  log("*Problem enabling "+rid);
+                }
+              });
+            });
           } else {
             log("*Problem registering "+rid);
           }
@@ -61,10 +105,28 @@ $(document).ready(function() {
       }
     } else {
       log("Creating owner Pico");
-      createOwnerPico(function(eci){
+      createOwnerPico(function(id,eci){
         log("Registering rulesets");
-        installRuleset("io.picolabs.pico",function(){
-          installRuleset("io.picolabs.visual_params",function(){
+        installAndAddRuleset("io.picolabs.pico",id,function(){
+          installAndAddRuleset("io.picolabs.visual_params",id,function(){
+            log("Sending event pico/root_created");
+            $.getJSON("/sky/event/"+eci+"/19/pico/root_created",
+              {"id":id,"eci":eci},function(d){
+                if (d && d.directives) {
+                  log("Event pico/root_created processed");
+                } else {
+                  log("*Problem with event pico/root_created");
+                }
+            });
+            log("Sending event visual/update");
+            $.getJSON("/sky/event/"+eci+"/31/visual/update",
+              {"dname":"OwnerPico","color":"#87cefa"},function(d){
+                if (d && d.directives) {
+                  log("Event visual/update processed");
+                } else {
+                  log("*Problem with event visual/update");
+                }
+            });
           });
         });
       });
