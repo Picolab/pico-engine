2688fe02c8bae45470a11f7bd4cb7964351850dd
Matthew Wright <matthew@smallhelm.com>
2016-10-05 14:06:50 -0500

    cleanup

--- packages/node-pico-engine-core/src/modules/event.js
+++ packages/node-pico-engine-core/src/modules/event.js
@@ -1,34 +1,32 @@
 var getArg = require("../getArg");
 
-var eventGetAttr = function(ctx, args){
-  var name = getArg(args, "name", 0);
-  return ctx.event.attrs[name];
-};
-
-var attrMatches = function(ctx, args){
-  var pairs = getArg(args, "pairs", 0);
-  var matches = [];
-  var i, attr, m, pair;
-  for(i = 0; i < pairs.length; i++){
-    pair = pairs[i];
-    attr = ctx.event.attrs[pair[0]];
-    m = pair[1].exec(attr || "");
-    if(!m){
-      return undefined;
+var fns = {
+  attr: function(ctx, args){
+    var name = getArg(args, "name", 0);
+    return ctx.event.attrs[name];
+  },
+  attrMatches: function(ctx, args){
+    var pairs = getArg(args, "pairs", 0);
+    var matches = [];
+    var i, attr, m, pair;
+    for(i = 0; i < pairs.length; i++){
+      pair = pairs[i];
+      attr = ctx.event.attrs[pair[0]];
+      m = pair[1].exec(attr || "");
+      if(!m){
+        return undefined;
+      }
+      matches.push(m[1]);
     }
-    matches.push(m[1]);
+    return matches;
+  },
+  send: function(ctx, args){
+    //TODO
   }
-  return matches;
 };
 
 module.exports = {
   get: function(ctx, id){
-    if(id === "attr"){
-      return eventGetAttr;
-    }else if(id === "attrMatches"){
-      return attrMatches;
-    }else if(id === "send"){
-      return function(){};
-    }
+    return fns[id];
   }
 };
