9a090382e96c9a823ea15b35d5bdbe524e9836f4
Matthew Wright <matthew@smallhelm.com>
2017-02-08 13:07:01 -0600

    enabling CORS

--- src/index.js
+++ src/index.js
@@ -92,6 +92,11 @@ PicoEngine({
   });
 
   var app = express();
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   app.use(express.static(path.resolve(__dirname, "..", "public")));
   app.use(bodyParser.json({type: "application/json"}));
   app.use(bodyParser.urlencoded({type: "application/x-www-form-urlencoded", extended: false}));
