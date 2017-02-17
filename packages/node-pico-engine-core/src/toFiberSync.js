f55f1b624d0d2b82b466795be19f95ecb7ad2552
Matthew Wright <matthew@smallhelm.com>
2017-02-17 17:30:31 -0600

    rm toFiberSync

--- packages/node-pico-engine-core/src/toFiberSync.js
+++
@@ -1,8 +0,0 @@
-var Future = require("fibers/future");
-
-module.exports = function(fn){
-    var fnF = Future.wrap(fn);
-    return function(){
-        return fnF.apply(this, arguments).wait();
-    };
-};
