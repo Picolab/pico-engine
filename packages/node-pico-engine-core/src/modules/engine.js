e80f2751e8501bfa850225ce352bb2dd0b3c56f1
Matthew Wright <matthew@smallhelm.com>
2017-02-22 16:59:16 -0600

    better error messages on engine:installRuleset

--- packages/node-pico-engine-core/src/modules/engine.js
+++ packages/node-pico-engine-core/src/modules/engine.js
@@ -6,11 +6,11 @@ var getArg = require("../getArg");
 var installRulesetAndValidateIds = function(db, pico_id, rid, callback){
     db.getPico(pico_id, function(err, pico){
         if(err) return callback(err);
-        if(!pico) return callback(new Error("invalid pico_id"));
+        if(!pico) return callback(new Error("Invalid pico_id: " + pico_id));
 
         db.hasEnabledRid(rid, function(err, has){
             if(err) return callback(err);
-            if(!has) return callback(new Error("invalid rid"));
+            if(!has) return callback(new Error("This rid is not found and/or enabled: " + rid));
 
             db.addRuleset({
                 pico_id: pico_id,
@@ -23,15 +23,15 @@ var installRulesetAndValidateIds = function(db, pico_id, rid, callback){
 var fns = {
     newPico: cocb.toYieldable(function(ctx, args, callback){
         var opts = getArg(args, "opts", 0);
-        return ctx.db.newPico(opts, callback);
+        ctx.db.newPico(opts, callback);
     }),
     removePico: cocb.toYieldable(function(ctx, args, callback){
         var id = getArg(args, "id", 0);
-        return ctx.db.removePico(id, callback);
+        ctx.db.removePico(id, callback);
     }),
     newChannel: cocb.toYieldable(function(ctx, args, callback){
         var opts = getArg(args, "opts", 0);
-        return ctx.db.newChannel(opts, callback);
+        ctx.db.newChannel(opts, callback);
     }),
     installRuleset: cocb.toYieldable(function(ctx, args, callback){
         var opts = getArg(args, "opts", 0);
@@ -60,8 +60,11 @@ var fns = {
         ctx.db.findRulesetsByURL(uri, function(err, results){
             if(err) return callback(err);
             var rids = _.uniq(_.map(results, "rid"));
+            if(_.size(rids) === 0){
+                return callback(new Error("No rid found for given url: " + uri));
+            }
             if(_.size(rids) !== 1){
-                return callback(new Error("Could not find the one ruleset"));
+                return callback(new Error("More than one rid found for the given url: " + rids.join(" , ")));
             }
             doIt(_.head(rids));
         });
