50c008ebd3797de137b2c439bc07b8b6bf2e462c
Matthew Wright <matthew@smallhelm.com>
2017-03-16 10:50:19 -0500

    src/tests startups the engine, leveldown and sets up the server

--- src/tests.js
+++ src/tests.js
@@ -1,1 +1,26 @@
-console.log("passed :)");
+var path = require("path");
+var tempfs = require("temp-fs");
+var setupServer = require("./setupServer");
+var startPicoEngine = require("./startPicoEngine");
+
+
+tempfs.mkdir({
+    dir: path.resolve(__dirname, ".."),
+    prefix: "pico-engine_test",
+    recursive: true,//It and its content will be remove recursively.
+    track: true//Auto-delete it on fail.
+}, function (err, dir) {
+    if(err) throw err;//fail the test
+
+    //try setting up the engine including registering rulesets
+    startPicoEngine(dir.path, function(err, pe){
+        if(err) throw err;//fail the test
+
+        //setup the server without throwing up
+        setupServer(pe);
+
+        console.log("passed!");
+        dir.unlink();
+        process.exit(0);//success
+    });
+});
