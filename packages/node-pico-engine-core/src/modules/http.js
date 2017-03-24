46b3cccf7c01a00b1d99e1af1c608594014cbf17
Matthew Wright <matthew@smallhelm.com>
2017-03-24 02:10:50 -0500

    http:<method>(...)

--- packages/node-pico-engine-core/src/modules/http.js
+++ packages/node-pico-engine-core/src/modules/http.js
@@ -3,41 +3,8 @@ var cocb = require("co-callback");
 var getArg = require("../getArg");
 var request = require("request");
 
-var doHTTP = function(opts, response_headers, callback){
-    request(opts, function(err, response, body){
-        if(err){
-            callback(err);
-            return;
-        }
-        var r = {
-            content: body,
-            content_type: response.headers["content-type"],
-            content_length: _.parseInt(response.headers["content-length"], 0) || 0,
-            status_code: response.statusCode,
-            status_line: response.statusMessage
-        };
-        _.each(response_headers, function(header){
-            r[header] = response.headers[header];
-        });
-        callback(void 0, r);
-    });
-};
-
-var fns = {
-    get: cocb.toYieldable(function(ctx, args, callback){
-        var url = getArg(args, "url", 0);
-        var params = getArg(args, "params", 1);
-        var headers = getArg(args, "headers", 2);
-        var response_headers = getArg(args, "response_headers", 3);
-
-        doHTTP({
-            method: "GET",
-            url: url,
-            qs: params || {},
-            headers: headers || {}
-        }, response_headers, callback);
-    }),
-    post: cocb.toYieldable(function(ctx, args, callback){
+var mkMethod = function(method){
+    return cocb.toYieldable(function(ctx, args, callback){
         var url = getArg(args, "url", 0);
         var params = getArg(args, "params", 1);
         var headers = getArg(args, "headers", 2);
@@ -46,7 +13,7 @@ var fns = {
         var credentials = getArg(args, "credentials", 5);
 
         var opts = {
-            method: "POST",
+            method: method,
             url: url,
             qs: params || {},
             headers: headers || {},
@@ -62,8 +29,33 @@ var fns = {
             opts.auth = credentials;
         }
 
-        doHTTP(opts, response_headers, callback);
-    })
+        request(opts, function(err, res, body){
+            if(err){
+                callback(err);
+                return;
+            }
+            var r = {
+                content: body,
+                content_type: res.headers["content-type"],
+                content_length: _.parseInt(res.headers["content-length"], 0) || 0,
+                status_code: res.statusCode,
+                status_line: res.statusMessage
+            };
+            _.each(response_headers, function(header){
+                r[header] = res.headers[header];
+            });
+            callback(void 0, r);
+        });
+    });
+};
+
+var fns = {
+    get: mkMethod("GET"),
+    post: mkMethod("POST"),
+    put: mkMethod("PUT"),
+    patch: mkMethod("PATCH"),
+    "delete": mkMethod("DELETE"),
+    head: mkMethod("HEAD"),
 };
 
 module.exports = {
