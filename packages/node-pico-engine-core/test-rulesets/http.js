05a10d3dcc2727c050a4a69ce3f8295da4954ec8
Matthew Wright <matthew@smallhelm.com>
2017-03-30 14:42:23 -0500

    http rename params to qs

--- packages/node-pico-engine-core/test-rulesets/http.js
+++ packages/node-pico-engine-core/test-rulesets/http.js
@@ -9,6 +9,27 @@ module.exports = {
       ctx.scope.set("r", ctx.getArg(ctx.args, "r", 0));
       return yield ctx.callKRLstdlib("delete", yield ctx.callKRLstdlib("set", ctx.scope.get("r"), "content", yield ctx.callKRLstdlib("decode", yield ctx.callKRLstdlib("get", ctx.scope.get("r"), ["content"]))), ["content_length"]);
     }));
+    ctx.scope.set("doPost", ctx.KRLClosure(ctx, function* (ctx) {
+      ctx.scope.set("base_url", ctx.getArg(ctx.args, "base_url", 0));
+      ctx.scope.set("to", ctx.getArg(ctx.args, "to", 1));
+      ctx.scope.set("msg", ctx.getArg(ctx.args, "msg", 2));
+      var actions = [{
+          "action": function* (ctx) {
+            return yield (yield ctx.modules.get(ctx, "http", "post"))(ctx, {
+              "0": yield ctx.callKRLstdlib("+", ctx.scope.get("url"), "/msg.json"),
+              "body": {
+                "To": ctx.scope.get("to"),
+                "Msg": ctx.scope.get("msg")
+              }
+            });
+          }
+        }];
+      var r = [];
+      var i;
+      for (i = 0; i < actions.length; i++)
+        r.push(yield actions[i].action(ctx));
+      return r;
+    }));
   },
   "rules": {
     "http_get": {
@@ -34,13 +55,74 @@ module.exports = {
         "fired": function* (ctx) {
           ctx.scope.set("resp", yield (yield ctx.modules.get(ctx, "http", "get"))(ctx, {
             "0": ctx.scope.get("url"),
-            "params": { "foo": "bar" },
+            "qs": { "foo": "bar" },
             "headers": { "baz": "quix" }
           }));
           yield ctx.modules.set(ctx, "ent", "get_resp", yield ctx.scope.get("fmtResp")(ctx, [ctx.scope.get("resp")]));
         },
         "notfired": undefined,
         "always": undefined
+      }
+    },
+    "http_post": {
+      "name": "http_post",
+      "select": {
+        "graph": { "http": { "post": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function* (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [[
+              "expr_0",
+              "end"
+            ]]
+        }
+      },
+      "prelude": function* (ctx) {
+        ctx.scope.set("url", yield (yield ctx.modules.get(ctx, "event", "attr"))(ctx, ["url"]));
+      },
+      "action_block": {
+        "actions": [{
+            "action": function* (ctx) {
+              return yield (yield ctx.modules.get(ctx, "http", "post"))(ctx, {
+                "0": ctx.scope.get("url"),
+                "body": { "foo": "bar" }
+              });
+            }
+          }]
+      }
+    },
+    "http_post_action": {
+      "name": "http_post_action",
+      "select": {
+        "graph": { "http": { "post_action": { "expr_0": true } } },
+        "eventexprs": {
+          "expr_0": function* (ctx) {
+            return true;
+          }
+        },
+        "state_machine": {
+          "start": [[
+              "expr_0",
+              "end"
+            ]]
+        }
+      },
+      "prelude": function* (ctx) {
+        ctx.scope.set("url", yield (yield ctx.modules.get(ctx, "event", "attr"))(ctx, ["url"]));
+      },
+      "action_block": {
+        "actions": [{
+            "action": function* (ctx) {
+              return yield ctx.scope.get("doPost")(ctx, {
+                "0": ctx.scope.get("url"),
+                "to": "bob",
+                "msg": "foobar"
+              });
+            }
+          }]
       }
     }
   }
--- packages/node-pico-engine-core/test-rulesets/http.krl
+++ packages/node-pico-engine-core/test-rulesets/http.krl
@@ -9,6 +9,13 @@ ruleset io.picolabs.http {
     fmtResp = function(r){
         r.set("content", r["content"].decode()).delete(["content_length"])
     }
+    doPost = defaction(base_url, to, msg){
+      http:post(url + "/msg.json")
+        with body = {
+          "To": to,
+          "Msg": msg
+        }
+    }
   }
   rule http_get {
     select when http get;
@@ -17,7 +24,7 @@ ruleset io.picolabs.http {
     }
     fired {
       resp = http:get(url) with
-          params = {
+          qs = {
             "foo": "bar"
           }
           headers = {
@@ -26,5 +33,25 @@ ruleset io.picolabs.http {
 
       ent:get_resp := fmtResp(resp)
     }
+  }
+  rule http_post {
+    select when http post;
+    pre {
+        url = event:attr("url")
+    }
+    http:post(url)
+      with body = {
+          "foo": "bar"
+      }
+  }
+  rule http_post_action {
+    select when http post_action;
+    pre {
+        url = event:attr("url")
+    }
+    doPost(url) with
+      to = "bob"
+      and
+      msg = "foobar"
   }
 }
