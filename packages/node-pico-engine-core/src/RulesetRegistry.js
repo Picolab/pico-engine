850b6f8280de4fe3efdb1a4f1696e2d34066f73f
Matthew Wright <matthew@smallhelm.com>
2017-05-19 15:27:56 -0500

    SalienceGraph

--- packages/node-pico-engine-core/src/RulesetRegistry.js
+++ packages/node-pico-engine-core/src/RulesetRegistry.js
@@ -1,9 +1,40 @@
 var _ = require("lodash");
 
+var SalienceGraph = function(){
+    var graph = {};
+    var put = function(rs){
+        del(rs.rid);//clear out the old one, if pressent
+        _.each(rs.rules, function(rule){
+            rule.rid = rs.rid;
+            _.each(rule.select && rule.select.graph, function(g, domain){
+                _.each(g, function(exprs, type){
+                    _.set(graph, [domain, type, rule.rid, rule.name], true);
+                });
+            });
+        });
+    };
+    var get = function(domain, type){
+        return _.get(graph, [domain, type], {});
+    };
+    var del = function(rid){
+        _.each(graph, function(data_d, domain){
+            _.each(data_d, function(data_t, type){
+                //clear out any old versions graph
+                _.unset(graph, [domain, type, rid]);
+            });
+        });
+    };
+    return Object.freeze({
+        put: put,
+        get: get,
+        del: del,
+    });
+};
+
 module.exports = function(){
 
     var rulesets = {};
-    var salience_graph = {};
+    var salience_graph = SalienceGraph();
     var keys_module_data = {};
 
     return Object.freeze({
@@ -35,34 +66,12 @@ module.exports = function(){
                 });
             }
 
-            //now setup `salience_graph` and `rulesets`
-            _.each(salience_graph, function(data_d, domain){
-                _.each(data_d, function(data_t, type){
-                    //clear out any old versions graph
-                    _.unset(salience_graph, [domain, type, rs.rid]);
-                });
-            });
-            _.each(rs.rules, function(rule){
-                rule.rid = rs.rid;
-                _.each(rule.select && rule.select.graph, function(g, domain){
-                    _.each(g, function(exprs, type){
-                        _.set(salience_graph, [domain, type, rule.rid, rule.name], true);
-                    });
-                });
-            });
+            salience_graph.put(rs);
             rulesets[rs.rid] = rs;
         },
         del: function(rid){
-            if(_.has(rulesets, rid)){
-                _.each(rulesets[rid].rules, function(rule){
-                    _.each(rule.select && rule.select.graph, function(g, domain){
-                        _.each(g, function(exprs, type){
-                            _.unset(salience_graph, [domain, type, rid]);
-                        });
-                    });
-                });
-                delete rulesets[rid];
-            }
+            salience_graph.del(rid);
+            delete rulesets[rid];
         },
         provideKey: function(rid, use_rid){
             if(_.has(keys_module_data, ["provided", use_rid, rid])){
@@ -76,23 +85,24 @@ module.exports = function(){
             return _.get(keys_module_data, ["used_keys", rid, key_id]);
         },
         salientRules: function(domain, type, ridFilter){
-            var to_run = _.get(salience_graph, [domain, type], {});
-
+            var to_run = salience_graph.get(domain, type);
             var rules_to_select = [];
             _.each(to_run, function(rules, rid){
                 if(!ridFilter(rid)){
                     return;
                 }
                 _.each(rules, function(is_on, rule_name){
-                    if(is_on){
-                        var rule = _.get(rulesets, [rid, "rules", rule_name]);
-                        if(rule){
-                            //shallow clone with it's own scope for this run
-                            rules_to_select.push(_.assign({}, rule, {
-                                scope: rulesets[rid].scope.push()
-                            }));
-                        }
+                    if(!is_on){
+                        return;
                     }
+                    var rule = _.get(rulesets, [rid, "rules", rule_name]);
+                    if(!rule){
+                        return;
+                    }
+                    //shallow clone with it's own scope for this run
+                    rules_to_select.push(_.assign({}, rule, {
+                        scope: rulesets[rid].scope.push()
+                    }));
                 });
             });
             return rules_to_select;
