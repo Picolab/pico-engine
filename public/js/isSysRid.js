787d40c86a17a41a681d16979903362c1118b685
Joshua Olson <0joshua.olson1@gmail.com>
2017-06-08 11:50:08 -0600

    Add oauth rid

--- public/js/isSysRid.js
+++ public/js/isSysRid.js
@@ -1,6 +1,7 @@
 window.isSysRid = function(rid){
   return rid === "Subscriptions"
       || rid === "io.picolabs.logging"
+      || rid === "io.picolabs.oauth_server"
       || rid === "io.picolabs.pico"
       || rid === "io.picolabs.visual_params";
 };
