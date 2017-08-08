160a30d90c6caf57a616e71560a933353931dcdc
Bruce Conrad <bruce_conrad@byu.edu>
2017-08-08 15:47:46 -0600

    guard against engine crash

--- src/startPicoEngine.js
+++ src/startPicoEngine.js
@@ -177,7 +177,7 @@ var setupLogging = function(pe){
     });
     pe.emitter.on("error", function(err, context){
         console.error("[ERROR]", context, err);
-        logEntry(context, err);
+        if(context) logEntry(context, err);
     });
     pe.emitter.on("episode_stop", function(context){
         var episode_id = context.txn_id;
