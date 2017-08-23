6957e510c2b25134f840cfb944be133bc7e02732
Matthew Wright <matthew@smallhelm.com>
2017-08-23 00:24:44 -0500

    better stub for wrangler tests

--- src/tests.js
+++ src/tests.js
@@ -1,5 +1,6 @@
 var test = require("tape");
 var path = require("path");
+var async = require("async");
 var tempfs = require("temp-fs");
 var setupServer = require("./setupServer");
 var startPicoEngine = require("./startPicoEngine");
@@ -12,8 +13,8 @@ var startTestServer = function(callback){
         prefix: "pico-engine_test",
         recursive: true,//It and its content will be remove recursively.
         track: !is_windows//Auto-delete it on fail.
-    }, function (err, dir) {
-        if(err) return callback(err);
+    }, function(err, dir){
+        if(err) throw err;//throw ensures process is killed with non-zero exit code
 
         //try setting up the engine including registering rulesets
         startPicoEngine({
@@ -21,28 +22,70 @@ var startTestServer = function(callback){
             home: dir.path,
             no_logging: true,
         }, function(err, pe){
-            if(err) return callback(err);
+            if(err) throw err;//throw ensures process is killed with non-zero exit code
 
             //setup the server without throwing up
             setupServer(pe);
 
-            callback(null, pe, function stopServer(){
-                if(!is_windows){
-                    dir.unlink();
-                }
-                process.exit(0);//success
+            pe.getRootPico(function(err, root_pico){
+                if(err) throw err;//throw ensures process is killed with non-zero exit code
+
+                callback(null, {
+                    pe: pe,
+                    root_eci: root_pico.eci,
+                    stopServer: function(){
+                        if(!is_windows){
+                            dir.unlink();
+                        }
+                    },
+                });
             });
         });
     });
 };
 
-test("wrangler", function(t){
-    startTestServer(function(err, pe, stopServer){
-        if(err) return t.end(err);
+test("pio-engine", function(t){
+    var pe;
+    var root_eci;
+    var stopServer;
+    async.series([
+        function(next){
+            startTestServer(function(err, tstserver){
+                if(err) return next(err);
+                pe = tstserver.pe;
+                root_eci = tstserver.root_eci;
+                stopServer = tstserver.stopServer;
+                next();
+            });
+        },
 
-        //TODO
+        ////////////////////////////////////////////////////////////////////////
+        //
+        // Wrangler tests
+        //
 
-        t.end();
+        function(next){
+            pe.runQuery({
+                eci: root_eci,
+                rid: "io.picolabs.pico",
+                name: "myself",
+                args: {},
+            }, function(err, data){
+                if(err) return next(err);
+
+                t.equals(data.eci, root_eci);
+
+                next();
+            });
+        },
+
+        //
+        // end Wrangler tests
+        //
+        ////////////////////////////////////////////////////////////////////////
+    ], function(err){
+        t.end(err);
         stopServer();
+        process.exit(err ? 1 : 0);//ensure server stops
     });
 });
