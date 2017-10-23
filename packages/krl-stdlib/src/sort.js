35fea598c55acbeecd01e43773ba5a68ce9a4b5f
Joshua Olson <0joshuaolson1@gmail.com>
2017-10-23 12:11:41 -0600

    Do iterative merge sort with better strides

--- packages/krl-stdlib/src/sort.js
+++ packages/krl-stdlib/src/sort.js
@@ -1,40 +1,48 @@
-//this mergesort is similar to https://github.com/calvinmetcalf/grin
-//for now, this relies on its caller using co-callback
-
-var merge = function*(comparator, start1, end, in_array, out_array){
-    var start2 = Math.floor((start1 + end)/2);//midpoint defining subarrays
-    var head1 = start1;
-    var head2 = start2;
-    var out = start1;
-    while(head1 < start2 && head2 < end){
-        out_array[out++] = in_array[
-            ((yield comparator(in_array[head1], in_array[head2])) <= 0)
-                ? head1++
-                : head2++
-        ];
-    }
-    while(head1 < start2){
-        out_array[out++] = in_array[head1++];
-    }
-    while(head2 < end){
-        out_array[out++] = in_array[head2++];
-    }
-};
-
+//for now, this mergesort relies on its caller using co-callback
 module.exports = function*(array, comparator){
-    var len = array.length;
-    var other = Array(len);
-    for(var stride=2; stride < len*2; stride*=2){
-        var start = 0;
-        var end = stride;
-        while(start < len){
-            yield merge(comparator, start, Math.min(end, len), array, other);
-            start = end;
-            end += stride;
-        }
+    var N = array.length;
+    if(N < 2){
+        return array;
+    }
+    var other = Array(N);
+
+    //alternative: var strideLen = N / Math.abs(1 << (Math.ceil(Math.log2(N)) - 1));
+    var strideLen = N;
+    while(strideLen > 1){
+        strideLen /= 2;
+    }
+
+    //alternative: first strideLen *= 2 and do a pre-pass for size-2 strides only
+    do{
+        var stride = strideLen;
+        var read1 = 0;
+        do{
+            var startRead2 = Math.ceil(stride);
+            var read2 = startRead2;
+            var mergeEnd = Math.ceil(stride + strideLen);
+            stride += 2 * strideLen;
+            var write = read1;
+            while(read1 < startRead2 && read2 < mergeEnd){
+                other[write++] = array[
+                    (yield comparator(array[read1], array[read2])) <= 0
+                        ? read1++
+                        : read2++
+                ];
+            }
+            while(read1 < startRead2){
+                other[write++] = array[read1++];
+            }
+            while(read2 < mergeEnd){
+                other[write++] = array[read2++];
+            }
+            read1 = mergeEnd;
+        }while(stride < N);
+
         var temp = array;
         array = other;
         other = temp;
-    }
+        strideLen *= 2;
+    }while(strideLen < N);
+
     return array;
 };
