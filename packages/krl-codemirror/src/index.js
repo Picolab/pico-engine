bf96f3fc4b0995f41a7c6d1fe44ad8acaa6a3aab
Matthew Wright <matthew@smallhelm.com>
2017-12-07 17:05:10 -0600

    krl-codemirror first draft for krl mode

--- packages/krl-codemirror/src/index.js
+++ packages/krl-codemirror/src/index.js
@@ -1,10 +1,15 @@
 require("codemirror/lib/codemirror.css");
+require("codemirror/addon/edit/matchbrackets");
 var CodeMirror = require("codemirror/lib/codemirror");
 
+require("codemirror/addon/mode/simple");
+require("./mode")(CodeMirror);
 
 window.KRL_EDITOR = function(el){
     var editor = CodeMirror.fromTextArea(el, {
-        mode: 'text/x-krl',
+        mode: "text/x-krl",
         lineNumbers: true,
+        matchBrackets: true,
+        indentUnit: 4,
     });
 };
