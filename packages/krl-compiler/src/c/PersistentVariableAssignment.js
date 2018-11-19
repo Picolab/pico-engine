e52e092be0d57f2f215874871b656beaedc123d7
Matthew Wright <matthew@smallhelm.com>
2018-11-19 17:11:58 -0600

    compile time performance hint to leverage ent/app indexes - for #425

--- packages/krl-compiler/src/c/PersistentVariableAssignment.js
+++ packages/krl-compiler/src/c/PersistentVariableAssignment.js
@@ -6,6 +6,23 @@ module.exports = function (ast, comp, e) {
     throw comp.error(ast.left.loc, 'PersistentVariableAssignment - only works on ent:* or app:* variables')
   }
 
+  // Performance Hint
+  // ent:v := ent:v.put(k, d)
+  // - should be -
+  // ent:v{k} := d
+  if (ast.right.type === 'Application' &&
+    ast.right.callee.type === 'MemberExpression' &&
+    ast.right.callee.object.type === 'DomainIdentifier'
+  ) {
+    const leftPVar = ast.left.domain + ':' + ast.left.value
+    const rightPVar = ast.right.callee.object.domain + ':' + ast.right.callee.object.value
+    if (leftPVar === rightPVar) {
+      if (ast.right.callee.property.type === 'Identifier' && ast.right.callee.property.value === 'put') {
+        comp.warn(ast.right.callee.object.loc, 'Performance Hint: to leverage indexes use `' + leftPVar + '{key} := value` instead of .put(key, value)')
+      }
+    }
+  }
+
   var valueToStore = comp(ast.right)
 
   var key = e('str', ast.left.value, ast.left.loc)
