41a70cbb29eb60863e7b135396a1f33ca8337cd0
Matthew Wright <matthew@smallhelm.com>
2019-02-18 17:21:14 -0600

    upgrade krl-editor config and nearley parser

--- packages/krl-editor/ace-webworker-loader.js
+++ packages/krl-editor/ace-webworker-loader.js
@@ -1,43 +1,47 @@
-var path = require("path");
-var webpack = require("webpack");
-var MemoryFS = require("memory-fs");
+var path = require('path')
+var webpack = require('webpack')
+var MemoryFS = require('memory-fs')
 
+var base_worker = require('brace/worker/json').src
 
-var base_worker = require("brace/worker/json").src;
+module.exports = function (content, map, meta) {
+  var callback = this.async()
 
-module.exports = function(content, map, meta){
-    var callback = this.async();
+  var memFS = new MemoryFS()
 
-    var memFS = new MemoryFS();
-
-    var compiler = webpack({
-        entry: this.resourcePath,
-        output: {
-            path: "/",
-            filename: "bundle.js",
-        },
-        plugins: [
-            new webpack.LoaderOptionsPlugin({
-                minimize: true
-            }),
-            new webpack.optimize.UglifyJsPlugin({
-                sourceMap: true
-            }),
-        ],
-    });
-
-    compiler.outputFileSystem = memFS;
-    compiler.run(function(err, stats){
-        if(err){
-            callback(err);
-            return;
+  var compiler = webpack({
+    mode: 'production',
+    output: {
+      path: '/',
+      filename: 'bundle.js'
+    },
+    entry: this.resourcePath,
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          strictModuleExceptionHandling: true
         }
-        var worker_src = "";
-        worker_src += base_worker + "\n\n";
-        worker_src += memFS.readFileSync("/bundle.js", "utf8");
+      })
+    ]
 
-        var src = "";
-        src += "module.exports=" + JSON.stringify(worker_src);
-        callback(null, src);
-    });
-};
+  })
+
+  compiler.outputFileSystem = memFS
+  compiler.run(function (err, stats) {
+    if (err) {
+      callback(err)
+      return
+    }
+    if (stats.hasErrors()) {
+      callback(stats.toJson().errors)
+      return
+    }
+    var worker_src = ''
+    worker_src += base_worker + '\n\n'
+    worker_src += memFS.readFileSync('/bundle.js', 'utf8')
+
+    var src = ''
+    src += 'module.exports=' + JSON.stringify(worker_src)
+    callback(null, src)
+  })
+}
