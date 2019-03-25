22b3bfaf589007a01f705b33875f7df5073a9da6
Matthew Wright <matthew@smallhelm.com>
2019-03-25 15:11:08 -0500

    better method for creating UI channel

--- packages/pico-engine-next/src/index.ts
+++ packages/pico-engine-next/src/index.ts
@@ -1,9 +1,42 @@
-import { PicoFramework } from "pico-framework";
+import {
+  PicoFramework,
+  ChannelConfig,
+  RulesetInstance,
+  ChannelReadOnly
+} from "pico-framework";
 import { inputToConf, PicoEngineSettings } from "./configuration";
 import { server } from "./server";
 import makeDir from "make-dir";
-import { ChannelConfig } from "pico-framework/dist/src/Channel";
+import { RulesetContext } from "pico-framework/dist/src/RulesetContext";
 const leveldown = require("leveldown");
+
+async function upsertChannel(
+  ctx: RulesetContext,
+  conf: ChannelConfig
+): Promise<ChannelReadOnly> {
+  if (!conf.tags || conf.tags.length === 0) {
+    throw new TypeError("upsertChannel needs tags set");
+  }
+  const searchTags = conf.tags
+    .slice(0)
+    .sort()
+    .join(",");
+  let uiChannel = ctx.pico().channels.find(chann => {
+    return (
+      searchTags ===
+      chann.tags
+        .slice(0)
+        .sort()
+        .join(",")
+    );
+  });
+  if (uiChannel) {
+    await ctx.putChannel(uiChannel.id, conf);
+  } else {
+    uiChannel = await ctx.newChannel(conf);
+  }
+  return uiChannel;
+}
 
 export async function startEngine(settings?: PicoEngineSettings) {
   const conf = inputToConf(settings);
@@ -16,10 +49,21 @@ export async function startEngine(settings?: PicoEngineSettings) {
   pf.addRuleset({
     rid: "io.picolabs.next",
     version: "0.0.0",
-    init(ctx) {
+    async init(ctx): Promise<RulesetInstance> {
+      const uiChannel = await upsertChannel(ctx, {
+        tags: ["engine", "ui"],
+        eventPolicy: { allow: [{ domain: "engine-ui", name: "*" }], deny: [] },
+        queryPolicy: {
+          allow: [{ rid: "io.picolabs.next", name: "pico" }],
+          deny: []
+        }
+      });
       return {
         event(event) {},
         query: {
+          uiECI() {
+            return uiChannel.id;
+          },
           pico(args) {
             return ctx.pico();
           }
@@ -30,7 +74,7 @@ export async function startEngine(settings?: PicoEngineSettings) {
   // TODO need to add ALL rulesets used in db before start
   await pf.start();
   await pf.rootPico.install("io.picolabs.next", "0.0.0");
-  let uiChannel = Object.values(pf.rootPico.channels).find(chann => {
+  const uiChannel = await Object.values(pf.rootPico.channels).find(chann => {
     return (
       "engine,ui" ===
       chann.tags
@@ -39,24 +83,11 @@ export async function startEngine(settings?: PicoEngineSettings) {
         .join(",")
     );
   });
-  const uiChanConf: ChannelConfig = {
-    tags: ["engine", "ui"],
-    eventPolicy: { allow: [{ domain: "engine-ui", name: "*" }], deny: [] },
-    queryPolicy: {
-      allow: [{ rid: "io.picolabs.next", name: "pico" }],
-      deny: []
-    }
-  };
-  if (uiChannel) {
-    await pf.rootPico.putChannel(uiChannel.id, uiChanConf);
-  } else {
-    uiChannel = await pf.rootPico.newChannel(uiChanConf);
-  }
 
   console.log(`Starting pico-engine-NEXT ${conf.version}`);
   console.log(conf);
 
-  const app = server(pf, conf, uiChannel.id);
+  const app = server(pf, conf, uiChannel ? uiChannel.id : "");
   await new Promise((resolve, reject) =>
     app.listen(conf.port, (err: any) => (err ? reject(err) : resolve()))
   );
