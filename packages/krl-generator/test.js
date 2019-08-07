2b42793e2115e0635d2e45ad98ac60aecabbddf1
Matthew Wright <matthew@smallhelm.com>
2019-08-07 13:26:26 -0600

    fixing krl-generator tests and switching to use ava

---
+++ packages/krl-generator/test.js
@@ -0,0 +1,26 @@
+const _ = require('lodash')
+const fs = require('fs')
+const generator = require('./')
+const parser = require('krl-parser')
+const path = require('path')
+const test = require('ava')
+
+const filesDir = path.resolve(__dirname, '../../test-rulesets')
+
+test('generator', async function(t){
+  const files = await fs.promises.readdir(filesDir)
+
+  for(const file of files){
+    if (!/\.krl$/.test(file)) {
+      continue
+    }
+    const src = await fs.promises.readFile(path.resolve(filesDir, file), 'utf8')
+    const expected = _.filter(src.split('\n'), function (line) {
+      return line.trim().indexOf('//') !== 0
+    }).join('\n').trim()
+
+    const out = generator(parser(src, { filename: file }))
+
+    t.is(expected, out, file)
+  }
+})
